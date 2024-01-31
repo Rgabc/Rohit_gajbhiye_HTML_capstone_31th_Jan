30bac684b6c2fac88a50a26cfe6c109cea4fd91d
Rgabc <rohitgajbhiye4@gmail.com>
2024-01-31 13:21:02 +0530

    capston

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -41,26 +41,27 @@ const Contact=()=>{
     return(<>
     <NavBar/>
     
-    <div className="contact">
-
-    <form onSubmit={submitHandler}>
-    <label htmlFor="name">Name</label><br/>
-    <input type="text" name="name" onChange={(e)=>{setFullName(e.target.value)}} value={fullname}/><br/>
-    <label htmlFor="email">email</label><br/>
-    <input type="text" name="email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/><br/>
-    {/* <label htmlFor="password">Password</label><br/>
-    <input type="text" name="password" onChange={(e)=>{setPassword(e.target.value)}}value={password}/><br/> */}
-    <label htmlFor="number">Mobile</label><br/>
-    <input type="number" name="number" onChange={(e)=>{setNumber(e.target.value)}} value={number}/><br/>
-    <label htmlFor="add">Address</label><br/>
-    <textarea onChange={(e)=>{setAdd(e.target.value)}}value={add}></textarea><br/>
-    <button type="submit" className="btn btn-success">submit</button>
-    
-</form>
-
-    </div>
     
 <div className="contact1">
+    
+<div className="contact">
+
+<form onSubmit={submitHandler}>
+<label htmlFor="name">Name</label><br/>
+<input type="text" name="name" onChange={(e)=>{setFullName(e.target.value)}} value={fullname}/><br/>
+<label htmlFor="email">email</label><br/>
+<input type="text" name="email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/><br/>
+{/* <label htmlFor="password">Password</label><br/>
+<input type="text" name="password" onChange={(e)=>{setPassword(e.target.value)}}value={password}/><br/> */}
+<label htmlFor="number">Mobile</label><br/>
+<input type="number" name="number" onChange={(e)=>{setNumber(e.target.value)}} value={number}/><br/>
+<label htmlFor="add">Address</label><br/>
+<textarea onChange={(e)=>{setAdd(e.target.value)}}value={add}></textarea><br/>
+<button type="submit" className="btn btn-success">submit</button>
+
+</form>
+
+</div>
     
 
 
