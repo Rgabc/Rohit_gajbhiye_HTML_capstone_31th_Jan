c6876689a433bd2c9c03ebd8a5b7848e2a7257e3
Rgabc <rohitgajbhiye4@gmail.com>
2024-02-02 10:59:22 +0530

    require

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -48,15 +48,15 @@ const Contact=()=>{
 
 <form onSubmit={submitHandler}>
 <label htmlFor="name">Name</label><br/>
-<input type="text" name="name" onChange={(e)=>{setFullName(e.target.value)}} value={fullname}/><br/>
+<input type="text" required name="name" onChange={(e)=>{setFullName(e.target.value)}} value={fullname}/><br/>
 <label htmlFor="email">email</label><br/>
-<input type="text" name="email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/><br/>
+<input type="text" required  name="email" onChange={(e)=>{setEmail(e.target.value)}} value={email}/><br/>
 {/* <label htmlFor="password">Password</label><br/>
 <input type="text" name="password" onChange={(e)=>{setPassword(e.target.value)}}value={password}/><br/> */}
 <label htmlFor="number">Mobile</label><br/>
-<input type="number" name="number" onChange={(e)=>{setNumber(e.target.value)}} value={number}/><br/>
+<input type="number" required  name="number" onChange={(e)=>{setNumber(e.target.value)}} value={number}/><br/>
 <label htmlFor="add">Address</label><br/>
-<textarea onChange={(e)=>{setAdd(e.target.value)}}value={add}></textarea><br/>
+<textarea required  onChange={(e)=>{setAdd(e.target.value)}}value={add}></textarea><br/>
 <button type="submit" className="btn btn-success">submit</button>
 
 </form>
